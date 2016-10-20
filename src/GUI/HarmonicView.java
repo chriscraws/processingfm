915831e6479cdf7738918116f3a32f88b3b4cb18
Christopher Crawford <clc3@princeton.edu>
2016-10-20 03:28:40 -0700

    HarmonicView uses NumberView for ratio control now

--- src/GUI/HarmonicView.java
+++ src/GUI/HarmonicView.java
@@ -8,8 +8,7 @@ public class HarmonicView extends View {
 
     private WaveformView waveformView;
     private RotaryView ampKnobView;
-    private RotaryView ratioKnobView;
-    private NumberView numberView;
+    private NumberView ratioControl;
 
     public static float defaultHeight = 70f;
 
@@ -23,36 +22,29 @@ public class HarmonicView extends View {
         ampKnobView.setLabel("Amp");
         addChild(ampKnobView);
 
-        ratioKnobView = new RotaryView(270, 0, 20);
-        ratioKnobView.setLabel("Ratio");
-        addChild(ratioKnobView);
+        ratioControl = new NumberView(270, 0, 2);
+        addChild(ratioControl);
 
-        numberView = new NumberView(350, 0, 2);
-        addChild(numberView);
 
     }
 
     public void writeWave(float[] buffer) {
-        waveformView.writeWave(buffer);
-    }
+                                    waveformView.writeWave(buffer);
+                                                                   }
 
     public int getBufferLength() {
-        return waveformView.getBufferLength();
-    }
+                           return waveformView.getBufferLength();
+                                                                 }
 
     public void setAmp(float amp) {
-        ampKnobView.setValue(amp);
-    }
+                            ampKnobView.setValue(amp);
+                                                      }
 
     public float getAmp() {
-        return ampKnobView.getValue();
-    }
-
-    public void setRatio(float ratio) {
-        ratioKnobView.setValue(ratio);
-    }
+                    return ampKnobView.getValue();
+                                                  }
 
     public float getRatio() {
-        return ratioKnobView.getValue();
+        return ratioControl.getValue() / 100f;
     }
 }
